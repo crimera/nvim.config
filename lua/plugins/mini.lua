3da3078cef0b42cfb3438f5051ace879c9b23a1f
crimera <44558091+crimera@users.noreply.github.com>
2025-08-26 11:28:20 +0800

    center the mini pick window

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,29 @@
+local win_config = function()
+	local height = math.floor(0.618 * vim.o.lines)
+	local width = math.floor(0.618 * vim.o.columns)
+	return {
+		anchor = 'NW',
+		height = height,
+		width = width,
+		row = math.floor(0.3 * (vim.o.lines - height)),
+		col = math.floor(0.5 * (vim.o.columns - width)),
+	}
+end
+
 return {
-	{ 
-		"echasnovski/mini.pick", 
+	{
+		"echasnovski/mini.pick",
 		version = "*",
 		config = function()
-			require "mini.pick".setup()
+			require "mini.pick".setup({
+				window = {
+					config = win_config,
+				}
+			})
 
 			vim.keymap.set('n', '<C-p>', ":Pick files<CR>")
 			vim.keymap.set('n', '<leader>f', ":Pick grep_live<CR>")
-		end 
+			vim.keymap.set('n', '<leader> ', ":Pick buffers<CR>")
+		end
 	},
 }
